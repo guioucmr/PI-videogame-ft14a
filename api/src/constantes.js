756c65b9c59d3a9ba1a1dac74cc2c0d5a8d7341c
Guioucmr <torqui_chelo@hotmail.com>
2021-07-07 11:29:07 -0300

    back

--- api/src/constantes.js
+++ api/src/constantes.js
@@ -3,7 +3,7 @@ const {API_KEY} = process.env;
 
 const BASE_URL = `https://api.rawg.io/api/games?key=${API_KEY}`
 const GENRE_URL = `https://api.rawg.io/api/genres?key=${API_KEY}`
-const SEARCH_URL = 'https://api.rawg.io/api/games?search'
+const SEARCH_URL = 'https://api.rawg.io/api/games/'
 
 module.exports={
     BASE_URL,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -44,12 +44,102 @@ router.get('/videogames', async(req, res, next) =>{
         
     }
 })
-router.get('/videogames/:id',async(req,res)=> {
-    const {id}=req.params
-    const forId =  await axios.get( `${BASE_URL}`)
-    const videos = forId.data.result.filter(p=>p.id===id)
-    res.json(videos)
-})
+router.get('/videogames/:id', async (req, res) => {
+    let {id} = req.params
+    try {
+      const response = await axios.get(`${SEARCH_URL}${id}?key=${API_KEY}`)
+      const games = {
+        name: response.data.name,
+        description: response.data.description,
+        background_image: response.data.background_image,
+        released: response.data.released,
+        rating: response.data.rating,
+        platforms: response.data.platforms,
+        id: response.data.id,
+        genres: response.data.genres
+      }
+      return res.send(games);
+      } catch (error) {
+      if(error.response?.status === 404) {
+        const game = await Videogame.findAll({
+          include:{
+            model: Genre,
+            attributes: ['id','name'],
+            through:{
+                attributes:[],
+            }
+          }
+        });
+        const filtered = await game.filter( e => e.id === id).shift()
+        if(filtered) return res.json(filtered);
+        return res.statusCode(404)
+      }
+      return res.status(500).json({error: 'Sorry... id not found'})
+    }
+  });
+
+  router.get('/genre',async(req,res,next)=> {
+    try {
+        const forGen =  await axios.get(`${GENRE_URL}`)
+        //console.log ("for_gen",forGen)
+        const temp = forGen.data.results.map(vg => {
+        return vg.name
+        })
+        //console.log("QUE TRAE", temp)
+        let newg = temp.map(string => {
+            return string && string.split(', ')
+        })
+        const cadena = newg.flat()
+        const filtrado = cadena.filter((string, i) => {
+            return cadena.indexOf(string) === i
+        })
+        const result = filtrado.map(mio =>{
+        return {
+            name: mio || 'name not exist'
+        }
+        }).sort((a, b) => {
+            if (a > b) return 1
+            if (a < b) return -1
+            return 0
+        })
+        if(!result.length){
+            await Genre.bulkCreate(result)
+        }
+        return res.send(result)
+    
+            
+        } catch (error) {next(error)
+            }
+    })
+
+
+//   router.get('/genre', async (req, res) => {
+//     try {
+//         const genres = await Genre.findAll()
+//         res.send(genres)
+//     } catch (error) {
+//         console.log(error)
+//     }
+//});
+
+    router.post('/videogame', async (req, res) => {
+        let {name, description, released, rating, platforms, genres} = req.body
+        
+          if(!name || !description || !platforms) {
+            return res.status(400).json({error: 'Sorry... all parameters are required'})
+          }
+          let newGame = await Videogame.create({
+            name: name,
+            description: description,
+            released: released,
+            rating: rating,
+            platforms: platforms,
+          })
+          console.log(newGame)
+          await newGame.setGenres(genres)
+          return res.send(newGame);
+      });
+
 // 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
